5a0a367e432ecd1d5b2212dc69be6839df0eb3b3
WMXPY <wm@mengw.io>
2019-07-05 10:01:13 -0500

    1.12 release, add migrate function

--- src/declare.ts
+++ src/declare.ts
@@ -13,14 +13,18 @@ export type SudooExpressRequest = {
     valid: boolean;
     readonly info: Record<string, any>;
 } & Request;
+
 export type SudooExpressResponse = {
     readonly agent: SudooExpressResponseAgent;
 } & Response;
+
 export type SudooExpressNextFunction = () => void;
+
 export type SudooExpressHandler = (req: SudooExpressRequest, res: SudooExpressResponse, next: SudooExpressNextFunction) => void;
 
 export type SudooExpressErrorInfo = {
     readonly code: number;
     readonly message: string;
 };
+
 export type SudooExpressErrorHandler = (code: number, error: Error) => SudooExpressErrorInfo;
